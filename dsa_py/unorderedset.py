d24183680f31ac40ff74d218a9644d3ce5047da7
tunnu-sahni <tsahni313@gmail.com>
2026-03-19 20:57:50 +0530

    added unorderedset.py

---
+++ dsa_py/unorderedset.py
@@ -0,0 +1,113 @@
+# s = {10, 20, 30, 40, 50}
+
+# print(s)
+# print(type(s))
+# print(len(s))
+
+
+# t = (10, 20, 30)
+# print(t)
+
+
+# s = {30, 49,59,60}
+# print(s)
+# s.add(40)
+# print(s)
+
+
+
+# s = {1,2,3,4,5}
+# print(s)
+# s.remove(3)
+# print(s)
+
+
+#insert(add)
+
+# s = {1,2,3,4,5}
+# s.add(6)
+# print(s)
+
+
+# R = {10, 20, 30, 40}
+# R.add(50)
+# print(R)
+# R.remove(20)
+# print(R)
+
+
+
+# #delete(remove)
+
+
+# s = {1,3,4,5}
+# s.remove(3)
+# print(s)
+
+# m = {20, 30, 40, 50}
+# m.remove(30)
+# print(m)
+
+
+# s = {1, 2, 3, 4,5}
+# s.discard(3)
+# print(s)
+
+
+#check element(fast search)
+
+# s = {10, 20, 30}
+# if 20 in s:
+#     print("20 is present in the set")
+
+
+# s = {3,4,5}
+# if 3 in s:
+#     print("3 is present in the set")
+
+
+
+#loop in set
+
+# s = {10,20,30,40}
+# for x in s:
+#     print(x)
+
+
+# t = {1,2,3,4,5}
+# for s in t:
+#     print(s)
+
+
+
+# union
+
+# a = {1,2,3}
+# b = {4,5,6}
+# print(a.union(b))
+# print(a | b)
+
+
+# intersection
+
+
+# print(a.intersection(b))
+# print(a & b)
+# print(a - b)
+
+
+
+# arr = [1,2,2,3,4,4,5]
+# unique = set(arr)
+# print(unique)
+
+
+
+
+#cbegin 
+
+
+s = {1,2,3}
+
+for x in s:
+    print(x)
